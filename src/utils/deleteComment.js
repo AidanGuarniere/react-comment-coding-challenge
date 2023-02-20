d1dba4718be423baab4b442e3cab441c7d4e128b
aidan <aidanguarniere@gmail.com>
2023-02-19 17:31:53 -0800

    debugged deleteComment.js so users may also delete their replies to other comments

--- src/utils/deleteComment.js
+++ src/utils/deleteComment.js
@@ -1,22 +1,3 @@
-function isChildOfComment(parentCommentId, comment, currentUser) {
-  if (comment.id === parentCommentId) {
-    // If this is the parent comment, check if the author is the same as the current user
-    if (comment.user.id === currentUser.id) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    // If this is a reply to the parent comment, recursively check its children
-    for (let i = 0; i < comment.children.length; i++) {
-      if (isChildOfComment(parentCommentId, comment.children[i], currentUser)) {
-        return true;
-      }
-    }
-    return false;
-  }
-}
-
 export function deleteComment(
   comments,
   commentId,
@@ -27,10 +8,16 @@ export function deleteComment(
   if (parentCommentId === null) {
     // If the parentCommentId is null, the comment to be deleted is a parent comment
     return comments.filter((comment) => {
-      console.log(comment.id, commentId)
       if (comment.id === commentId) {
-        return false;
+        // If this is the comment to be deleted, check if the author is the same as the current user
+        if (comment.user.id === currentUser.id) {
+          return false;
+        } else {
+          // If the author is not the same as the current user, delete the comment and its children
+          return false;
+        }
       } else {
+        // If this is not the comment to be deleted, recursively delete its children
         return {
           ...comment,
           children: deleteComment(
@@ -49,11 +36,22 @@ export function deleteComment(
         // If this is the parent comment, filter out the reply to be deleted
         return {
           ...comment,
-          children: comment.children.filter(
-            (childComment) =>
-              childComment.id !== commentId ||
-              childComment.user.id === currentUser.id
-          ),
+          children: comment.children.filter((childComment) => {
+            if (childComment.id === commentId) {
+              // If this is the reply to be deleted, check if the author is the same as the current user or if the parent comment is authored by the current user
+              if (
+                childComment.user.id === currentUser.id ||
+                comment.user.id === currentUser.id
+              ) {
+                return false;
+              } else {
+                return true;
+              }
+            } else {
+              // If this is not the reply to be deleted, keep the comment
+              return true;
+            }
+          }),
         };
       } else {
         // If this is not the parent comment, recursively delete the reply from its children
